cb504ab5d69003934bcc533999da9217184bc265
victor hugo <victor.hr.santiago@outlook.com.br>
2022-11-06 11:20:19 -0300

    alterando squelizeCli

--- config/sequelizeCli.js
+++ config/sequelizeCli.js
@@ -1,9 +1,9 @@
 module.exports = {
     development: {
       dialect: 'postgres',
-      host: 'onebitflix-db.cgrbfxibbh8a.us-east-1.rds.amazonaws.com',
+      host: 'db-nova.cgrbfxibbh8a.us-east-1.rds.amazonaws.com',
       port: '5432',
-      database: 'onebitflix_db',
+      database: 'onebitflix_development',
       username: 'onebitflix',
       password: 'onebitflix'
     }
