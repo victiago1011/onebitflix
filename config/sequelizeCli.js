5b449e189d6ccbf8b22f4f97bafd333f1b33e2e4
victor hugo <victor.hr.santiago@outlook.com.br>
2022-11-08 20:24:12 -0300

    adicionando .env

--- config/sequelizeCli.js
+++ config/sequelizeCli.js
@@ -1,10 +1,10 @@
-module.exports = {
-    development: {
-      dialect: 'postgres',
+const { Pool } = require("pg")
+require("dotenv").config()
+
+module.exports = new Pool({    
       host: 'db-nova.cgrbfxibbh8a.us-east-1.rds.amazonaws.com',
       port: '5432',
       database: 'onebitflix_db',
       username: 'onebitflix',
       password: 'onebitflix'
-    }
-  }
+  });
